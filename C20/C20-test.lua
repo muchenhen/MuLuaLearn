42eeed1a6481155f47c0c169200b225ea24f7cc9
muchenhen <44308876+muchenhen@users.noreply.github.com>
2021-09-13 10:00:53 +0800

    更新

--- C20/C20-test.lua
+++ C20/C20-test.lua
@@ -1,5 +1,6 @@
 local Set = {}
 
+-- 为Set添加new方法根据指定列表添加一个新的集合
 function Set.new(l)
     local set = {}
     for _, v in ipairs(l) do
@@ -15,6 +16,8 @@ for key, value in pairs(s1) do
     print(key, value)
 end
 
+for key, value in pairs(s2) do
+    print(key, value)
+end
 
-print(s1)
-print(s2)
+-- 创建元表，为Set添加元方法

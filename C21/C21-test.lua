aae62223f53701f19702475772c59506f122f29c
muchenhen <44308876+muchenhen@users.noreply.github.com>
2021-10-18 15:23:52 +0800

    测试lua面向对象

--- C21/C21-test.lua
+++ C21/C21-test.lua
@@ -17,12 +17,12 @@ function Account:Cost(n)
 end
 
 local a1 = Account:new()
-print(a1.have)
+print("a1.have", a1.have)
 a1:Cost(10)
-print(a1.have)
+print("a1.have", a1.have)
 
 local a2 = Account:new()
-print(a2.have)
+print("a2.have", a2.have)
 
 -- SpecialAccount继承了Account的方法
 SpeciAccount = Account:new()
@@ -36,5 +36,31 @@ local A = Account:new()
 local spA = SpeciAccount:new()
 A:Cost(110)
 spA:Cost(110)
-print(A.have)
-print(spA.have)
+print("A.have", A.have)
+print("spA.have", spA.have)
+
+function class(className, baseCalss)
+    local newCalss = {}
+    local newMate = {}
+    if baseCalss then
+        newMate.__index = baseCalss
+        setmetatable(newCalss, newMate)
+    else
+        newMate.__index = newCalss
+        setmetatable(newCalss, newMate)
+    end
+    newCalss.__name = className
+    newCalss.__uid = className .. "_" .. tostring(newCalss)
+    return newCalss
+end
+
+VIP = class("VIP", SpeciAccount)
+local V1 = VIP:new()
+function VIP:Save(n)
+    self.have = self.have + n * 2
+end
+V1:Save(5)
+V1:Cost(110)
+print("V1.have", V1.have)
+print("V1.__uid", V1.__uid)
+print("V1.__name", V1.__name)
